b2a46bdab688455ac9283651a2abf2edc92b8d04
Mikhail Duhno <misha.duhno@mail.ru>
2025-10-01 00:51:36 +0300

    chapter_2-task_1. Выполнил задачу 1 из второго раздела

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,32 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
 
-}
+export const sortStrings = (arr, param = 'asc') => {
+  if (arr.length > 1) { // Если у нас в массиве всего 1 элемент, нет смысла его сортировать, просто возвращаем его
+    let arrCopy = [...arr];
+
+    arrCopy = arrCopy.sort((a, b) => {
+      let changePos;
+
+      // В зависимости от param записываем в changePos нужное значение
+      if (param === 'asc') {
+        changePos = a.toLowerCase().localeCompare(b.toLowerCase());
+      }
+      else {
+        changePos = a.toLowerCase().localeCompare(b.toLowerCase()) * -1;
+      }
+
+      // Нужно чтобы слова с заглавными первыми буквами шли первее чем со строчными
+      if (changePos) {
+        return changePos;
+      } else if (a[0] < b[0]) {
+        return -1;
+      }
+    });
+
+    return arrCopy; // Возвращаем отсортированный массив
+  }
+
+  return arr;
+};
